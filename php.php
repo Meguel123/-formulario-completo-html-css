3bb54241de8367059ede39d9be4701fbc01ade57
Miguel <miguel.perassoli@gmail.com>
2025-04-03 00:08:54 -0300

    finalizado

---
+++ php.php
@@ -0,0 +1,33 @@
+
+<div style= "">
+<?php
+
+
+
+$idade = $_GET["idade"];
+$nome = $_GET["nome"];
+$altura = $_GET["altura"];
+$lingua = $_GET["linguagem"]
+$email = $_GET["email"];
+$dt = $_GET["dataNascimento"]
+$Nivel = $_GET["nivelHtml"]
+$S = $_GET["sistemaOperacional"]
+
+echo "nome ". $nome;
+echo "<br>";
+echo "altura ". $altura;
+echo "<br>";
+echo "idade ". $idade;
+echo "<br>";
+echo "email ". $email;
+echo "<br>";
+echo "data de Nascimento ". $dt;
+echo "<br>";
+echo "Nivel html ". $Nivel;
+echo "<br>";
+echo "Sistema Operacional ". $S;
+
+
+
+?>
+</div>
